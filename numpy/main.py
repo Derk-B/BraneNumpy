ff1f880d669a68bbb7e162018937f551849de4e6
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2024-03-26 21:14:39 +0100

    Added 4 more functions

--- numpy/main.py
+++ numpy/main.py
@@ -13,9 +13,39 @@ def test():
     
 def zeros():
     shape = ast.literal_eval(os.getenv("SHAPE").replace('"', ''))
-    arr = np.zeros(shape)
+    dtype = os.getenv("DTYPE").replace('"', '')
+    order = os.getenv("ORDER").replace('"', '')
+    arr = np.zeros(shape, dtype, order)
     yaml.dump({"output": arr})
-    
+
+def array_sum():
+    arr = os.getenv("ARRAY")
+    res = np.sum(ast.literal_eval(arr))
+    print(f"sum: {res}")
+
+def array_dot():
+    env1 = os.getenv("FIRST")
+    env2 = os.getenv("SECOND")
+    arr1 = ast.literal_eval(env1)
+    arr2 = ast.literal_eval(env2)
+    res = np.dot(arr1, arr2)
+    print(f"dot: {res}")
+
+def array_reshape():
+    env1 = os.getenv("ARRAY")
+    env2 = os.getenv("SHAPE")
+    arr = ast.literal_eval(env1)
+    shape = ast.literal_eval(env2)
+    res = np.reshape(arr, shape)
+    yaml.dump({'reshaped': res})
+
+def array_add():
+    env1 = os.getenv("FIRST")
+    env2 = os.getenv("SECOND")
+    arr1 = ast.literal_eval(env1)
+    arr2 = ast.literal_eval(env2)
+    res = np.add(arr1, arr2)
+    yaml.dump({'add': res})
 
 
 if __name__ == "__main__":
@@ -27,10 +57,18 @@ if __name__ == "__main__":
     # If it checks out, call the appropriate function
     command = sys.argv[1]
     if command == "test":
-            test()
+        test()
     elif command == "zeros":
-            zeros()
-
+        zeros()
+    elif command == 'sum':
+        array_sum()
+    elif command == 'dot':
+        array_dot()
+    elif command == 'reshape':
+        array_reshape()
+    elif command == 'add':
+        array_add()
+    
     # Done!
     # print("output: '" + str(cmds) + "'")
     # print("output: '" + cmds + "'")
